0d78a3798cc11c08438a273dfa39f581741b3a37
Federico-Taborda <panterx.95@gmail.com>
2024-12-29 02:20:06 -0300

    Middlewares deleted

--- src/v1/middlewares/user.middleware.js
+++
@@ -1,9 +0,0 @@
-export const middleware = (req, res, next) => {
-    const { saludo }  = req.body;
-    if(saludo === "Hola"){
-        console.log("Paso por el middleware")
-        next()
-    }else{
-        res.status(401).send({message: "No tienes permisos"})
-    }
-}
--- src/v1/routes/user.routes.js
+++ src/v1/routes/user.routes.js
@@ -1,12 +1,8 @@
 import { Router } from 'express';
-import { middleware } from '../middlewares/user.middleware.js';
 import userController from '../../controllers/userController.js';
 
 // Se crea un router para manejar las rutas de los usuarios
 const userRouter = Router();
-
-// Se aplica el middleware a todas las rutas de userRouter
-userRouter.use(middleware)
 
 userRouter.get('/', userController.getAllUsers);
 
